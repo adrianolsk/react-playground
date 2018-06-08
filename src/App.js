786a5243c2a935f31079418a8d1270824dc98ac8
Adriano Skroch <adriano.skroch@gmail.com>
2018-06-08 11:02:00 -0300

    fix: missing css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Clientes from "./pages/Clientes";
